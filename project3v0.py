253a96c430f105556aba1b9b67c7353579aeb3eb
shiokuo <55738202+shiokuo@users.noreply.github.com>
2019-12-31 13:30:03 +0800

    Add files via upload
    ?

---
+++ project3v0.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_digits
+digits = load_digits()
+digits.keys()
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(6,6)) # figure size in inches
+fig.subplots_adjust(left=0,right=1,bottom=0,top=1,hspace=0.05,wspace=0.05)
+
+# plot the digits
+for i in range(64):
+    ax = fig.add_subplot(8,8,i+1,xticks=[],yticks=[])
+    ax.imshow(digits.images[i],cmap=plt.cm.binary,interpolation='nearest')
+    # label the image with the target value
+    ax.text(0,7,str(digits.target[i]))
+# Quickly classify the digits using a random forest
+from sklearn.model_selection import train_test_split
+Xtrain,Xtest,ytrain,ytest = train_test_split(digits.data,digits.target,random_state=0)
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(n_estimators=1000)
+model.fit(Xtrain,ytrain)
+ypred = model.predict(Xtest)
+from sklearn import metrics
+print(metrics.classification_report(ypred,ytest))
